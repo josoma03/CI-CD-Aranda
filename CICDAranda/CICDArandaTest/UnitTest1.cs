a070d260f3ce5cac0ca2afc5a32cfcf666f68551
Jhonattan Solarte <josoma03@gmail.com>
2020-07-16 17:05:58 -0500

    Proyectos test

---
+++ CICDAranda/CICDArandaTest/UnitTest1.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+
+namespace CICDArandaTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+        [Test]
+        public void Test2()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ CICDAranda/CICDArandaTestSelenium/TestClass.cs
@@ -0,0 +1,38 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using System.Collections;
+using System.Collections.Generic;
+using CICDArandaTestSelenium.Base;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace CICDArandaTestSelenium
+{
+    [TestFixture]
+    public class TestClass: BaseClass
+    {
+        [Test, Category("Regression Testing")]
+        public void TestMethod()
+        {
+            
+            IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
+            emailTextField.SendKeys("Aranda Software");
+            
+
+            // TODO: Add your test code here
+            var answer = 42;
+            Assert.That(answer, Is.EqualTo(42), "Some useful error message");
+        }
+
+
+        [Test, Category("Smoke Testing")]
+        public void TestMethod2()
+        {
+
+            IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
+            emailTextField.SendKeys("Aranda Software");
+        }
+    }
+}
+ 
